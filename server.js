54ae5bb756d5e8ee586b7e44e2c228022140bf48
Marco Pellegrini <marco.pellegrini1996@gmail.com>
2025-03-27 15:19:53 +0200

    added basic endpoints for users

---
+++ server.js
@@ -0,0 +1,17 @@
+const fastify = require('fastify')({ logger: true })
+
+fastify.register(require('./routes/users'))
+
+const PORT = 5000
+
+const start = async () => {
+	try {
+		await fastify.listen({ port: PORT, host: '0.0.0.0' })
+		console.log(`Server listening on http://localhost:${PORT}`)
+	} catch (error) {
+		fastify.log.error(error)
+		process.exit(1)
+	}
+}
+
+start()
