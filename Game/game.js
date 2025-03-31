43a5091ca564f8d808547d9d583abdbf3199c335
Peter Leander <pleander@student.hive.fi>
2025-03-31 14:43:33 +0300

    Add draft for the game field
    Simple design with two paddles that can be moved with the w/s and
    UP/DOWN keys.

--- Game/game.js
+++ Game/game.js
@@ -1,18 +1,45 @@
-const height = 200;
-const width = 500;
+const width = 1000;
+const height = (3/4) * width;
+const paddle_height = 20;
 
 class Game {
-    constructor() {
+	constructor() {
 		this.players = [];
 		this.state = {
 			ball: {x: width/2, y: height/2, vx: 2, vy: 1},
-			paddles: [0, 0]
+			paddles: [0, 0] // offset from vertical center point
 		};
 	};
 
 	getState() {
 		return this.state;
 	}
+
+	updatePaddle1(change) {
+		this.updatePaddle(change, 0);
+	}
+
+	updatePaddle2(change) {
+		this.updatePaddle(change, 1);
+	}
+
+	updatePaddle(change, paddleInd) {
+
+		if (change < 0) {
+			let paddle_center = height / 2 + this.state.paddles[paddleInd];
+			let paddle_top = paddle_center - paddle_height;
+			if (paddle_top - change > 0) {
+				this.state.paddles[paddleInd] += change;
+			}
+		}
+		if (change > 0) {
+			let paddle_center = height / 2 + this.state.paddles[paddleInd];
+			let paddle_bottom = paddle_center + paddle_height;
+			if (paddle_bottom + change < height)
+				this.state.paddles[paddleInd] += change;
+		}
+	}
 }
+		
 
 export {Game}
--- Game/render.js
+++ Game/render.js
@@ -1,5 +1,72 @@
 import { Game } from './game.js';
 
+let controls_p1 = {up: 0, down: 0};
+let controls_p2 = {up: 0, down: 0};
+
 const game = new Game();
 
-console.log(game.getState());
+let board = document.querySelector('.board');
+let paddle = document.querySelector('.paddle');
+let paddle_1 = document.querySelector('.p1_paddle');
+let paddle_2 = document.querySelector('.p2_paddle');
+
+const centerHeight = board.getBoundingClientRect().height / 2;
+const paddleHeight = paddle.getBoundingClientRect().height;
+
+document.addEventListener('keydown', (e) => {
+	if (e.key === 'w') {
+		controls_p1.up = 1;
+	}
+	else if (e.key === 's') {
+		controls_p1.down = 1;
+	}
+	else if (e.key === 'ArrowUp') {
+		controls_p2.up = 1;
+	}
+	else if (e.key === 'ArrowDown') {
+		controls_p2.down = 1;
+	}
+});
+
+document.addEventListener('keyup', (e) => {
+	if (e.key === 'w') {
+		controls_p1.up = 0;
+	}
+	else if (e.key === 's') {
+		controls_p1.down = 0;
+	}
+	else if (e.key === 'ArrowUp') {
+		controls_p2.up = 0;
+	}
+	else if (e.key === 'ArrowDown') {
+		controls_p2.down = 0;
+	}
+});
+
+let state = game.getState();
+
+function render() {
+	state = game.getState();
+	paddle_1.style.top = centerHeight + state.paddles[0];
+	paddle_2.style.top = centerHeight + state.paddles[1];
+}
+
+function updatePaddles() {
+	if (controls_p1.up == 1 && controls_p1.down == 0) {
+		game.updatePaddle1(-10);
+	}
+	else if (controls_p1.up == 0 && controls_p1.down == 1) {
+		game.updatePaddle1(10);
+	}
+	if (controls_p2.up == 1 && controls_p2.down == 0) {
+		game.updatePaddle2(-10);
+	}
+	else if (controls_p2.up == 0 && controls_p2.down == 1) {
+		game.updatePaddle2(10);
+	}
+}
+
+setInterval(updatePaddles, 10);
+setInterval(render, 10);
+
+
