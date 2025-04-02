b23d07f5a4b34508341f81921ae69f4710743a91
Peter Leander <pleander@student.hive.fi>
2025-04-02 15:30:03 +0300

    Add core functionality and refactor using Canvas
    Refactored game to Canvas instead to make it more extendable. The game
    is now a bit more flexible and fetches e.g. game settings from the
    backend. It also features a ball and keeps score. Yay!

--- Game/game.js
+++ Game/game.js
@@ -1,45 +1,175 @@
-const width = 1000;
-const height = (3/4) * width;
-const paddle_height = 20;
+const board_width= 800;
+const board_height = 600;
+const paddle_height = 100;
+const paddle_width	= 10;
+const paddle_wall_dist = 20; // distance from wall to center of paddle
+const ball_radius = 10;
+
+class Player {
+	constructor(player_id) {
+		this.score = 0;
+		this.id = player_id;
+		//this.socket?
+	}
+}
+
+class Paddle {
+	constructor(x_init, y_init) {
+		this.y_offset = 0;
+		this.initial_pos = [x_init, y_init]; // center of rect block
+	}
+
+	getSides() {
+		const yTop = this.initial_pos[1] + this.y_offset - paddle_height / 2;
+		const yBot = this.initial_pos[1] + this.y_offset + paddle_height / 2;
+		const xLeft = this.initial_pos[0] - paddle_width / 2;
+		const xRight = this.initial_pos[0] + paddle_width / 2;
+
+		return {
+			"yTop": yTop,
+			"yBot": yBot,
+			"xLeft": xLeft,
+			"xRight": xRight
+		}
+	}
+};
 
 class Game {
 	constructor() {
-		this.players = [];
-		this.state = {
-			ball: {x: width/2, y: height/2, vx: 2, vy: 1},
-			paddles: [0, 0] // offset from vertical center point
+		this.finished_rounds = 0;
+		this.players = {
+			"player_1": new Player(1),
+			"player_2": new Player(2)
+		}
+		this.objects = {
+			ball: {x: board_width/2, y: board_height/2, vx: 2, vy: 1},
+			left_paddle: new Paddle(paddle_wall_dist, board_height / 2),
+			right_paddle: new Paddle(board_width - paddle_wall_dist, board_height / 2)
 		};
-	};
-
-	getState() {
-		return this.state;
 	}
 
-	updatePaddle1(change) {
-		this.updatePaddle(change, 0);
+	get state() {
+		return {
+			"objects": this.objects,
+			"finished_rounds": this.finished_rounds,
+			"players": this.players
+		}
 	}
 
-	updatePaddle2(change) {
-		this.updatePaddle(change, 1);
+	getSettings() {
+		return {
+			"board_width": board_width,
+			"board_height": board_height,
+			"paddle_height": paddle_height,
+			"paddle_width": paddle_width,
+			"paddle_wall_dist": paddle_wall_dist,
+			"ball_radius": ball_radius
+		}
 	}
 
-	updatePaddle(change, paddleInd) {
+	resetGame() {
+		this.objects.left_paddle.y_offset = 0;
+		this.objects.right_paddle.y_offset = 0;
+		// Todo: maybe separate ball function with reset method
+		this.objects.ball.x = board_width / 2;
+		this.objects.ball.y = board_height / 2;
+		this.objects.ball.vx = 2;
+		this.objects.ball.vy = 1;
+	}
+
+	movePaddle1(dir) {
+		let change = 0;
+		if (dir === "up") {
+			change = -5
+		}
+		else if (dir === "down") {
+			change = 5;
+		}
+		this.updatePaddle(change, this.objects.left_paddle);
+	}
+
+	movePaddle2(dir) {
+		let change = 0;
+		if (dir === "up") {
+			change = -5
+		}
+		else if (dir === "down") {
+			change = 5;
+		}
+		this.updatePaddle(change, this.objects.right_paddle);
+	}
+
+	updatePaddle(change, paddle) {
+
+		const sides = paddle.getSides();
 
 		if (change < 0) {
-			let paddle_center = height / 2 + this.state.paddles[paddleInd];
-			let paddle_top = paddle_center - paddle_height;
-			if (paddle_top - change > 0) {
-				this.state.paddles[paddleInd] += change;
+			if (sides.yTop - change >= 0) {
+				paddle.y_offset += change;
 			}
 		}
 		if (change > 0) {
-			let paddle_center = height / 2 + this.state.paddles[paddleInd];
-			let paddle_bottom = paddle_center + paddle_height;
-			if (paddle_bottom + change < height)
-				this.state.paddles[paddleInd] += change;
+			if (sides.yBot + change <= board_height)
+				paddle.y_offset += change;
 		}
+	}
+
+	endRound(winner_player) {
+		winner_player.score += 1;
+		this.resetGame()
+	}
+
+	moveBall() {
+		let ball = this.objects.ball;
+
+		const lp_sides = this.objects.left_paddle.getSides();
+		const rp_sides = this.objects.right_paddle.getSides();
+		
+		// Hit right paddle
+		if (ball.vx > 0) {
+			let dx = rp_sides.xLeft - ball.x;
+			if (dx > 0 && dx <= ball_radius && (ball.y > rp_sides.yTop && ball.y < rp_sides.yBot)) {
+				ball.vx = -ball.vx;
+			}
+		}
+
+		// Hit left paddle
+		if (ball.vx < 0) {
+			let dx = ball.x - lp_sides.xRight;
+			if (dx > 0 && dx <= ball_radius && (ball.y > lp_sides.yTop && ball.y < lp_sides.yBot)) {
+				ball.vx = -ball.vx;
+			}
+		}
+
+		// Hit bottom of board
+		if (board_height - ball.y <= ball_radius) {
+			ball.vy = -ball.vy;
+		}
+		
+		// Hit top of board
+		if (ball.y <= ball_radius) {
+			ball.vy = -ball.vy;
+		}
+		
+		// Hit right wall
+		if (ball.x >= board_width) {
+			this.endRound(this.players.player_1);
+			return;
+		}
+		//
+		// Hit left wall
+		if (ball.x <= 0) {
+			this.endRound(this.players.player_2);
+			return;
+		}
+
+		ball.x += this.objects.ball.vx;
+		ball.y += this.objects.ball.vy;
+	}
+
+	refreshGame() {
+		this.moveBall();
 	}
 }
 		
-
 export {Game}
--- Game/render.js
+++ Game/render.js
@@ -1,17 +1,27 @@
 import { Game } from './game.js';
 
-let controls_p1 = {up: 0, down: 0};
-let controls_p2 = {up: 0, down: 0};
+// colors
+const BLACK = "#000000";
+const WHITE = "#ffffff";
 
 const game = new Game();
+const settings = game.getSettings();
 
-let board = document.querySelector('.board');
-let paddle = document.querySelector('.paddle');
-let paddle_1 = document.querySelector('.p1_paddle');
-let paddle_2 = document.querySelector('.p2_paddle');
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
 
-const centerHeight = board.getBoundingClientRect().height / 2;
-const paddleHeight = paddle.getBoundingClientRect().height;
+canvas.setAttribute("height", settings.board_height);
+canvas.setAttribute("width", settings.board_width);
+
+// settings
+const paddle_height = settings.paddle_height;
+const paddle_width = settings.paddle_width;
+const paddle_margin = settings.paddle_wall_dist;
+const paddle_start = (canvas.height / 2) - (paddle_height / 2);
+const ball_radius = settings.ball_radius;
+
+let controls_p1 = {up: 0, down: 0};
+let controls_p2 = {up: 0, down: 0};
 
 document.addEventListener('keydown', (e) => {
 	if (e.key === 'w') {
@@ -43,30 +53,75 @@ document.addEventListener('keyup', (e) => {
 	}
 });
 
-let state = game.getState();
+let state = game.state;
+
+function draw_paddle_1(offset) {
+	ctx.fillStyle = WHITE;
+	ctx.fillRect(paddle_margin, paddle_start + offset, paddle_width, paddle_height);
+}
+
+function draw_paddle_2(offset) {
+	ctx.fillStyle = WHITE;
+	ctx.fillRect(canvas.width - paddle_margin - paddle_width, paddle_start + offset, paddle_width, paddle_height);
+}
+
+function draw_ball(ball_state) {
+	ctx.fillStyle = WHITE;
+	ctx.beginPath();
+	ctx.arc(ball_state.x, ball_state.y, ball_radius, 0, Math.PI * 2, true);
+	ctx.fill();
+}
+
+function draw_scores(players) {
+	console.log(players);
+	ctx.font = "48px serif";
+	ctx.fillText(players['player_1'].score, settings.board_width / 4, 50);
+	ctx.fillText(players['player_2'].score, settings.board_width * (3/4), 50);
+}
+
+function draw_center_line()
+{
+	ctx.setLineDash([10, 10]);
+	ctx.strokeStyle = WHITE;
+	ctx.lineWidth = paddle_width / 2;
+	ctx.beginPath();
+	ctx.moveTo(canvas.width / 2, 0);
+	ctx.lineTo(canvas.width / 2, canvas.height);
+	ctx.stroke();
+}
 
 function render() {
-	state = game.getState();
-	paddle_1.style.top = centerHeight + state.paddles[0];
-	paddle_2.style.top = centerHeight + state.paddles[1];
+	state = game.state;
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	draw_center_line();
+	draw_scores(state.players);
+	draw_paddle_1(state.objects.left_paddle.y_offset);
+	draw_paddle_2(state.objects.right_paddle.y_offset);
+	draw_ball(state.objects.ball);
 }
 
 function updatePaddles() {
 	if (controls_p1.up == 1 && controls_p1.down == 0) {
-		game.updatePaddle1(-10);
+		game.movePaddle1("up");
 	}
 	else if (controls_p1.up == 0 && controls_p1.down == 1) {
-		game.updatePaddle1(10);
+		game.movePaddle1("down");
 	}
 	if (controls_p2.up == 1 && controls_p2.down == 0) {
-		game.updatePaddle2(-10);
+		game.movePaddle2("up");
 	}
 	else if (controls_p2.up == 0 && controls_p2.down == 1) {
-		game.updatePaddle2(10);
+		game.movePaddle2("down");
 	}
+}
+
+
+// Temporary function to refresh game
+function refreshGame() {
+	game.refreshGame();
 }
 
 setInterval(updatePaddles, 10);
 setInterval(render, 10);
-
+setInterval(refreshGame, 10);
 
