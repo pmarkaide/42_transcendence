4b246cfb52b31bf5bf95b7e410b308fe6dd1d561
Pablo Markaide <pablo.markaide@gmail.com>
2025-04-07 16:07:27 +0300

    feat: add dotenv package and check for variables

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,15 @@ const fastify = require('fastify')({
 
 const fastifyOAuth2 = require('@fastify/oauth2')
 
+require('dotenv').config();
+// Check credential works
+try {
+	require('dotenv').config();
+	console.log("Environment loaded. GOOGLE_CLIENT_ID exists:", !!process.env.GOOGLE_CLIENT_ID);
+  } catch (error) {
+	console.error("Error loading dotenv:", error.message);
+  }
+
 fastify.register(import('@fastify/swagger'), {
 	swagger: {
 		securityDefinitions: {
