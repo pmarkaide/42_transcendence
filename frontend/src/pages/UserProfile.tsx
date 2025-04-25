04dd2695d3c7448de01e55eb688aa4f9bcc275ae
Leonor Tu <jingyi.tu@outlook.com>
2025-04-25 16:55:30 +0300

    feat: add UserCard component and integrate it into UserProfile page; remove commented frontend service in docker-compose

--- frontend/src/pages/UserProfile.tsx
+++ frontend/src/pages/UserProfile.tsx
@@ -1,6 +1,28 @@
-const UserProfile = () => {
+import UserCard from './UserCard';
+
+interface UserProfileProps {
+  id: number;
+  username: string;
+  avatar: string;
+  online_status: string;
+};
+
+const UserProfile: React.FC<UserProfileProps> = ({
+  id,
+  username,
+  avatar,
+  online_status,
+}) => {
   return (
-    <div>UserProfile</div>
-  )
-}
-export default UserProfile
+    <div>
+      <UserCard
+        id={id}
+        username={username}
+        avatar={avatar}
+        online_status={online_status}
+      ></UserCard>
+    </div>
+  );
+};
+
+export default UserProfile;
